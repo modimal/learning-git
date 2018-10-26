0b156798becb976eb34c5ac093b80f98a845f6c4
Arsen Bekzhanov <liwidne@gmail.com>
2018-10-26 19:34:21 +0300

    Update hello_user.py
    Made hello human script

---
+++ hello_user.py
@@ -0,0 +1,7 @@
+#! usr/bin/env python3
+# coding: utf-8
+
+if __name__ == "__main__":
+    print("Hello human!")
+    user_name = input("What's your name? Enter here: ")
+    print("Nice to meet you {}".format(user_name))
